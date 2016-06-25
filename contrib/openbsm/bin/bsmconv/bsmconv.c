5a7426edadbff3c66bbbedaf1739ba13e31a0db5
Mateusz Piotrowski <mpp302@gmail.com>
2016-06-25 02:07:42 +0200

    Fix some issues with sbuf.
    Some records are still missing from the eventbuf.

--- contrib/openbsm/bin/bsmconv/bsmconv.c
+++ contrib/openbsm/bin/bsmconv/bsmconv.c
@@ -97,8 +97,18 @@ find_msg_field_end(struct sbuf *buf, const size_t pos)
 }
 
 static void
-process_event(const struct sbuf * const buf)
+process_event(struct sbuf *buf)
 {
+	int retval;
+
+	retval = sbuf_finish(buf);
+	if (retval == -1)
+		err(errno, "sbuf_finish");
+
+	assert(sbuf_len(buf) != -1);
+
+	debug("event: |%zu| (%.*s)", sbuf_len(buf), (int)sbuf_len(buf), sbuf_data(buf));
+
 	return;
 }
 
@@ -112,15 +122,19 @@ parse_record(struct sbuf * const eventbuf, struct sbuf *recordbuf,
 	size_t idlen;
 	char *recorddata;
 	char *iddata;
+	int retval;
 
 	assert(sbuf_len(idbuf) != -1);
 	assert(sbuf_len(recordbuf) != -1);
 
-	sbuf_done(recordbuf);
+	retval = sbuf_finish(recordbuf);
+	if (retval == -1)
+		err(errno, "sbuf_finish");
 	recordlen = sbuf_len(recordbuf);
 	recorddata = sbuf_data(recordbuf);
 
 	msgfieldpos = find_msg_field_position(recordbuf);
+	/* debug("len (%zu), data (%s)", recordlen, recorddata); */
 
 	/* Find the msg field. */
 	if (msgfieldpos == -1) {
@@ -128,10 +142,8 @@ parse_record(struct sbuf * const eventbuf, struct sbuf *recordbuf,
 		/*     to have any newlines. */
 		warnx("record's msg field not found; "
 		    "the records will be ignored");
-		warnx("the record looks like this: %.*s", recordlen,
+		warnx("the record looks like this: (%.*s)", recordlen,
 		    recorddata);
-		sbuf_clear(recordbuf);
-		return;
 	}
 	else {
 		msgfieldend = find_msg_field_end(recordbuf, msgfieldpos);
@@ -141,31 +153,38 @@ parse_record(struct sbuf * const eventbuf, struct sbuf *recordbuf,
 		if (sbuf_len(idbuf) == 0) {
 			recorddata = sbuf_data(recordbuf);
 			idlen = msgfieldend - msgfieldpos;
-			sbuf_bcat(idbuf, recorddata + msgfieldpos, idlen);
+			retval = sbuf_bcat(idbuf, recorddata + msgfieldpos, idlen);
+			if (retval == -1)
+				err(errno, "sbuf_bcat");
 			assert(sbuf_len(idbuf) == idlen);
-			sbuf_bcat(eventbuf, recorddata, recordlen);
-			return;
+			retval = sbuf_bcat(eventbuf, recorddata, recordlen);
+			if (retval == -1)
+				err(errno, "sbuf_bcat");
 		}
+		else {
+			idlen = sbuf_len(idbuf);
+			iddata = sbuf_data(idbuf);
 
-		idlen = sbuf_len(idbuf);
-		iddata = sbuf_data(idbuf);
+			/* This record is from the next event. */
+			if (strncmp(iddata, recorddata + msgfieldpos, idlen) != 0) {
+				/* Parse and print the current event. */
+				process_event(eventbuf);
 
-		/* This record is from the next event. */
-		if (strncmp(iddata, recorddata + msgfieldpos, idlen) != 0) {
-			/* Parse and print the current event. */
-			process_event(eventbuf);
-
-			/* Clean the event. */
-			sbuf_clear(eventbuf);
-			sbuf_clear(idbuf);
+				/* Clean the event. */
+				sbuf_clear(eventbuf);
+				sbuf_clear(idbuf);
+			}
+			/* Add the current record to the event. */
+			retval = sbuf_bcat(eventbuf, recorddata, recordlen);
+			if (retval == -1)
+				err(errno, "sbuf_bcat");
+			/* Separate the records with the EOS character. */
+			retval = sbuf_bcat(eventbuf, "\0", 1);
+			if (retval == -1)
+				err(errno, "sbuf_bcat");
 		}
-		/* Add the current record to the event. */
-		sbuf_bcat(eventbuf, recorddata, recordlen);
-		/* Separate the records with the EOS character. */
-		sbuf_bcat(eventbuf, "\0", 1);
-
-		sbuf_clear(recordbuf);
 	}
+	sbuf_clear(recordbuf);
 }
 
 int main()
@@ -180,7 +199,7 @@ int main()
 	ssize_t newlinepos;
 	ssize_t bytesread;
 	size_t offsetlen;
-	int resval;
+	int retval;
 
 	eventbuf = sbuf_new_auto();
 	if (eventbuf == NULL)
@@ -188,6 +207,10 @@ int main()
 
 	idbuf = sbuf_new_auto();
 	if (idbuf == NULL)
+		err(errno, "sbuf_new_auto");
+
+	inbuf = sbuf_new_auto();
+	if (inbuf == NULL)
 		err(errno, "sbuf_new_auto");
 
 	recordbuf = sbuf_new_auto();
@@ -203,15 +226,15 @@ int main()
 		if (bytesread == -1)
 			err(errno, "read");
 		else if (bytesread == 0) {
-			debug("end of file.");
+			debug("eof");
 			break;
 		}
 
-		inbuf = NULL;
-		sbuf_new(inbuf, readbuf, bytesread, SBUF_AUTOEXTEND);
-		if (inbuf == NULL)
-			err(errno, "sbuf_new");
-		if (sbuf_finish(inbuf) == -1)
+		retval = sbuf_bcat(inbuf, readbuf, bytesread);
+		if (retval == -1)
+			err(errno, "sbuf_bcat");
+		retval = sbuf_finish(inbuf);
+		if (retval == -1)
 			err(errno, "sbuf_finish");
 		assert(sbuf_done(inbuf) != 0);
 		indata = sbuf_data(inbuf);
@@ -222,17 +245,26 @@ int main()
 			assert(sbuf_data(inbuf)[newlinepos] == '\n');
 
 			offsetlen = newlinepos - offset;
-			resval = sbuf_bcat(recordbuf, indata + offset, offsetlen);
-			if (resval == -1)
+			retval = sbuf_bcat(recordbuf, indata + offset, offsetlen);
+			if (retval == -1)
 				err(errno, "sbuf_bcat");
+			retval = sbuf_finish(recordbuf);
+			if (retval == -1)
+				err(errno, "sbuf_finish");
 			offset += newlinepos + 1;
 			parse_record(eventbuf, recordbuf, idbuf);
 		}
 
-		offsetlen = sbuf_len(inbuf) - offset + 1;
-		resval = sbuf_bcat(recordbuf, indata + offset, offsetlen);
+		offsetlen = sbuf_len(inbuf) - offset;
+		retval = sbuf_bcat(recordbuf, indata + offset, offsetlen);
+		if (retval == -1)
+			err(errno, "sbuf_bcat");
 
-		sbuf_delete(inbuf);
+		sbuf_clear(inbuf);
+	}
+	assert(sbuf_len(eventbuf) != -1);
+	if (sbuf_len(eventbuf) != 0) {
+		process_event(eventbuf);
 	}
 	sbuf_delete(eventbuf);
 	sbuf_delete(recordbuf);
