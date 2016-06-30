59c3e3228cb6fb90df384ece63a9957a6b312290
Mateusz Piotrowski <mpp302@gmail.com>
2016-06-30 21:29:59 +0200

    Rewrite bsmconv.c.

--- contrib/openbsm/bin/bsmconv/bsmconv.c
+++ contrib/openbsm/bin/bsmconv/bsmconv.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <unistd.h> /* getopt(3) */
 
-/* #include "linau_event.h" */
-#include "linau_record.h"
+#include "linau.h"
 
 #include "pjdlog.h"
 
 int
 main(int argc, char *argv[]) {
-	/* struct linau_event * event; */
-	struct linau_record * record;
+	linau_event *event;
+	linau_record *record;
 	FILE * fp;
 	int debuglevel;
 
@@ -36,9 +35,12 @@ main(int argc, char *argv[]) {
 
 	fp = stdin;
 
-	while ((record = linau_record_fetch(fp)) != NULL) {
+	event = linau_event_create();
 
-	}
+	while ((record = linau_record_fetch(fp)) != NULL)
+		linau_event_add_record(event, record);
+
+	linau_event_print(event);
 
 	return (0);
 }
